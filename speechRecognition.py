6690334227846630a774d77d31da9d8e45bda67d
VIP <vipjohar@gmail.com>
2022-09-18 03:46:53 -0400

    Added speak (TTS)

--- speechRecognition.py
+++ speechRecognition.py
@@ -1,8 +1,10 @@
-import pyaudio
 import speech_recognition as sr
+import pyttsx3
+import win32api
 
 recog = sr.Recognizer()
-sound = ""
+engine = pyttsx3.init()
+
 
 def listen():
     with sr.Microphone() as source:
@@ -10,10 +12,44 @@ def listen():
 
     try:
         result = recog.recognize_google(sound)
-        return { "result": result };
+        return {"result": result}
 
     except sr.UnknownValueError:
-        return { "error": "Google Speech Recognition could not understand audio" };
+        return {"error": "Google Speech Recognition could not understand audio"}
 
     except sr.RequestError as e:
-        return { "error": "Could not request results from Google Speech Recognition service; {0}".format(e) };
+        return {"error": "Could not request results from Google Speech Recognition service; {0}".format(e)}
+
+
+def speak(text):
+    # # Language in which you want to convert
+    # language = 'en'
+
+    # myobj = gTTS(text=text, lang=language, slow=False)
+
+    # # Saving the converted audio in a mp3 file named
+    # # welcome
+    # myobj.save("text.mp3")
+
+    # # Playing the converted file
+    # system("text.mp3")
+
+    # getting details of current speaking rate
+    # rate = engine.getProperty('rate')
+    # print(rate)  # printing current voice rate
+    engine.setProperty('rate', 125)
+
+    # voices = engine.getProperty('voices')  # getting details of current voice
+    # print(voices)
+    # engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+    engine.setProperty('voice', engine.getProperty('voices')[1].id)
+
+    # getting to know current volume level (min=0 and max=1)
+    # volume = engine.getProperty('volume')
+    # print(volume)  # printing current volume level
+
+    engine.say(text)
+    engine.runAndWait()
+
+
+speak("Mubin we killed this. We're winning 2 grand")
