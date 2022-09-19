6fa5cfb99b02f97a63367c75563ebb138099153f
Nausher Rao <sherrao@Naushers-MBP.wlu.ca>
2022-09-19 11:47:49 -0400

    Make README look pretty

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# computer-speech
-Use speech recognition to convert speech to text and submit to Cohere API to generate a report based on keywords
+# That's Crazy!
+CLI program using speech recognition and NLP to summarize text, generate freestyle song lyrics, and create blog posts, THAT'S CRAZY!
+
+## How To Use
+- NOTE: Make sure you have Python 10 installed, as well as pip configured.
+
+- First, clone the repo locally using
+```git clone https://github.com/vipj/computer-speech```
+
+- Next, install all the dependencies using pip
+```pip install -r requirements.txt```
+OR
+```pip3 install -r requirements.txt```
+
+- Finally, use the help command to see a list of commands.
+```python main.py --help```
+OR
+```python3 main.py --help``
--- ml.py
+++ ml.py
@@ -1,7 +1,5 @@
-import cohere
-co = cohere.Client('tafccigkrHkGNU4KGeIx4Bk0xqjWQDe1xQZIGjg3')
-
-
+import cohere;
+co = cohere.Client('tafccigkrHkGNU4KGeIx4Bk0xqjWQDe1xQZIGjg3');
 
 def blog_post(title,audience,tone):
     prompt=f"""This program will generate an introductory paragraph to a blog post given a blog title, audience, and tone of voice.
@@ -23,7 +21,7 @@ First Paragraph:"""
     response = co.generate(
     model='xlarge',
     prompt=prompt,
-   
+
     max_tokens=100,
     temperature=0.8,
     k=0,
@@ -31,18 +29,18 @@ First Paragraph:"""
     frequency_penalty=0,
     presence_penalty=0,
     stop_sequences=["--"],
-    return_likelihoods='NONE')
-    return('Prediction: {}'.format(response.generations[0].text))
-    
+    return_likelihoods='NONE');
+    return('Prediction: {}'.format(response.generations[0].text));
+
 def freestyle_bot(first_line):
-    
+
     prompt=f"""This program will generate a hook to a song given the first line.
 --
 First Line: Hey, I just met you, and this is crazy
 Hook: Hey, I just met you, and this is crazy, But here's my number, so call me, maybe It's hard to look right at you, baby But here's my number, so call me, maybe Hey, I just met you, and this is crazy But here's my number, so call me, maybe And all the other boys try to chase me But here's my number, so call me, maybe
 --
 First Line: Sweet Caroline
-Hook: Sweet Caroline Good times never seemed so good I've been inclined. To believe they never would. To believe they never would, Oh no no   
+Hook: Sweet Caroline Good times never seemed so good I've been inclined. To believe they never would. To believe they never would, Oh no no
 --
 First Line: I'm on my way Driving at 90 down those country lanes
 Hook: I'm on my way Driving at 90 down those country lanes Singing to Tiny Dancer And I miss the way you make me feel, and it's real When we watched the sunset over the castle on the hill
@@ -58,7 +56,7 @@ Hook: 'Cause he seems like he's good for you And he makes you feel like you shou
 
 --
 First Line: Just gonna stand there and watch me burn
-Hook: Just gonna stand there and watch me burn? Well, that's alright, because I like the way it hurts Just gonna stand there and hear me cry? 
+Hook: Just gonna stand there and watch me burn? Well, that's alright, because I like the way it hurts Just gonna stand there and hear me cry?
 Well, that's alright, because I love the way you lie I love the way you lie I love the way you lie
 --
 First Line: I'm gonna pop some tags
@@ -71,14 +69,14 @@ First Line: But girl I can't feel my face
 Hook: But girl I can't feel my face What are we smokin' anyway? She said don't let the high go to waste But can you taste a little taste Novacane, baby, baby Novacane, baby, I want you
 --
 First Line: Yeah, I be yelling out money over everything
-Hook: Yeah, I be yelling out money over everything, money on my mind Then she wanna ask when it got so empty Tell her I apologize it happened 
+Hook: Yeah, I be yelling out money over everything, money on my mind Then she wanna ask when it got so empty Tell her I apologize it happened
 over time She says they missed the old drake, girl don't tempt me
 --
 First Line: Jetson came in with a FN Two girls with him
 Hook: Jetson came in with a FN Two girls with him, told me he ain't got a preference (yeah) Sat them both down and I asked a few questions Met last week and they already best friends (oh-ooh) Already best friends (friends) Met last week and they already best friends (best friends) Met last week and they already best friends (oh-oh)
 --
 First Line: {first_line}
-Hook:"""
+Hook:""";
     response = co.generate(
         model='xlarge',
         prompt = prompt,
@@ -89,8 +87,8 @@ Hook:"""
         frequency_penalty=0,
         presence_penalty=0,
         stop_sequences=["--"],
-        return_likelihoods='NONE')
-    return('Prediction: {}'.format(response.generations[0].text))
+        return_likelihoods='NONE');
+    return('Prediction: {}'.format(response.generations[0].text));
 
 
 
@@ -103,24 +101,24 @@ Passage: ArtificialIvan, a seven-year-old, London-based payment and expense mana
 
 TLDR: ArtificialIvan has raised $190 million in Series C funding.
 --
-Passage: I'm a server. I haven't worked since the 14th, but was officially laid off on the 18th. Where I am, temporary layoff is only 60 days. If we are not back by then, we lose our jobs permanently. It couldn't have come at a worse time since we have new management and there's been some murmurs about getting new staff. I was about to lose my apartment (unrelated) but my landlord phoned me up to let me know that he is no 
+Passage: I'm a server. I haven't worked since the 14th, but was officially laid off on the 18th. Where I am, temporary layoff is only 60 days. If we are not back by then, we lose our jobs permanently. It couldn't have come at a worse time since we have new management and there's been some murmurs about getting new staff. I was about to lose my apartment (unrelated) but my landlord phoned me up to let me know that he is no
 longer making me leave and not to worry about April rent. He also gave me $150 to go buy groceries. He's a bro. I'm trying to get through to EI, which is all online right now, but the site keeps crashing so I'm unable to make a claim right now.
 TLDR: I was about to lose my apartment (unrelated) but my landlord phoned me up to let me know that he is no longer making me leave. I'm trying to get through to EI, which is all online right now, but the site keeps crashing.
 --
-Passage: I was laid off from my 5 year graphic design job, working for a screen printing and promotional products company. About half of our business involves schools, sports, and events. Obviously none of that is happening currently, so our business has stagnated. They are going to 
-try to operate with a skeleton crew for the next couple months. The hope is to bring everyone back eventually, but I'm not optimistic.        
-TLDR: I was laid off from my 5 year graphic design job, working for a screen printing and promotional products company. They are going to try 
+Passage: I was laid off from my 5 year graphic design job, working for a screen printing and promotional products company. About half of our business involves schools, sports, and events. Obviously none of that is happening currently, so our business has stagnated. They are going to
+try to operate with a skeleton crew for the next couple months. The hope is to bring everyone back eventually, but I'm not optimistic.
+TLDR: I was laid off from my 5 year graphic design job, working for a screen printing and promotional products company. They are going to try
 to operate with a skeleton crew for the next couple months. The hope is to bring everyone back eventually, but I'm not optimistic.
 --
 Passage: My grandmother is from Ireland (I live in the US) and honestly probably should've kicked the bucket years ago. She absolutely hates England, and my brother and I have had a running joke for years that she's holding on through sheer spite to see the queen die first. I found out about the queen because he texted me saying "guess grandma is kicking the bucket next week"
-TLDR: He found out about it because he texted me saying "guess grandma is kicking the bucket next week". My grandmother is from Ireland and absolutely hates England, and my brother and I have had a running joke that she's holding on through sheer spite to see the queen die first.    
+TLDR: He found out about it because he texted me saying "guess grandma is kicking the bucket next week". My grandmother is from Ireland and absolutely hates England, and my brother and I have had a running joke that she's holding on through sheer spite to see the queen die first.
 --
-Passage: Butterfingers are super gross now, bought one out of a vending machine and thought it was bad, googled it to find a bunch of people complaining about it. How do you screw up so bad? It’s probably cheaper but I’d think you’d lose enough sales to counter that. I won’t buy one 
+Passage: Butterfingers are super gross now, bought one out of a vending machine and thought it was bad, googled it to find a bunch of people complaining about it. How do you screw up so bad? It’s probably cheaper but I’d think you’d lose enough sales to counter that. I won’t buy one
 again for sure.
 TLDR: Supermarket produce in general. Puts the cart before the horse and its worn people down until they don't know what 'real' food tastes like. Some go so far as to 'flavor inject' it after with industrial flavors, like 'purple' (artificial concord grape) like the Grapple.
 --
 Passage: {input}
-TLDR:"""
+TLDR:""";
     response = co.generate(
         model='xlarge',
         prompt= prompt,
@@ -131,8 +129,8 @@ TLDR:"""
         frequency_penalty=0,
         presence_penalty=0,
         stop_sequences=["--"],
-        return_likelihoods='NONE')
-    return('Prediction: {}'.format(response.generations[0].text))
+        return_likelihoods='NONE');
+    return('Prediction: {}'.format(response.generations[0].text));
 #freestyle_bot("Life is missery, I am sad, why do I love you so much")
 #summarizer("To be fair, you have to have a very high IQ to understand Rick and Morty. The humor is extremely subtle, and without a solid grasp of theoretical physics most of the jokes will go over a typical viewer's head. There's also Rick's nihilistic outlook, which is deftly woven into his characterisation - his personal philosophy draws heavily fromNarodnaya Volya literature, for instance. The fans understand this stuff; they have the intellectual capacity to truly appreciate the depths of these jokes, to realize that they're not just funny- they say something deep about LIFE.")
 #blog_post("elon musk","children","sarcastic")
